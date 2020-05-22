dec5868c65532cc5a1cc10ac504240c5ffab88d1
Dario <Dario@DESKTOP-21JCDOK>
2020-05-22 11:13:45 -0300

    Asunto: Inicializacion del Proyecto Responsable: Josue Dario Alfaro Task00: Inicializacion del Proyecto

---
+++ src/test/java/ar/edu/unju/fi/Alfaro3490Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alfaro3490Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
